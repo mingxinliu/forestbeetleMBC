0894af7b977cc512d04cff2a7093c2a202c97cc2
Mingxin Liu <liu_ming_xin@163.com>
2020-10-25 15:44:19 +1100

    Update lulu_CO1.R

--- lulu_CO1.R
+++ lulu_CO1.R
@@ -11,17 +11,12 @@ library(lulu)
 
 otutable <- read.csv(args[1], sep = '\t', header = TRUE, as.is = TRUE, row.names = 1)
 
-names(otutable) <- sub("*.CO1", "", names(otutable))
-
-names(otutable) <- sub("X", "site", names(otutable))
-
-otutable <- subset(otutable, select = -c(site130000.1051, site90000.1136))
+colnames(otutable) <- gsub(".CO1", "", gsub("X", "site", colnames(otutable)))
 
 matchlist <- read.table(args[2], header = FALSE, as.is = TRUE, stringsAsFactors = FALSE)
 
-lulu_result <- lulu(otutable, matchlist, minimum_ratio_type = "min", minimum_ratio = 1, minimum_match = 84, minimum_relative_cooccurence = 0.95)
+lulu_result <- lulu(otutable, matchlist, minimum_ratio_type = "min", minimum_ratio = 1, minimum_match = 90, minimum_relative_cooccurence = 0.95)
 
 write.csv(lulu_result$curated_otus, file = args[3])
 
 write.csv(lulu_result$curated_table, file = args[4])
-
